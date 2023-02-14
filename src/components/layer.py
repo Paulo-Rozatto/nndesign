efdd3803ae9426e3b389c932dff2e17cdd928017
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2023-02-14 20:02:52 -0300

    first networks

--- src/components/layer.py
+++ src/components/layer.py
@@ -2,15 +2,33 @@ import numpy as np
 
 class Layer:
     def __init__(self, neurons: list):
-        self.weights = np.array([n.get_weights() for n in neurons])
-        self.biases = np.array([n.get_bias() for n in neurons])
-        self.activations = [n.activation for n in neurons]
+        self._weights = np.array([n.get_weights() for n in neurons])
+        self._biases = np.array([n.get_bias() for n in neurons])
+        self._activations = [n.activation for n in neurons]
     
     def activate(self, inputs):
         inputs = np.array(inputs, dtype=np.float64)
-        product = np.dot(self.weights, inputs.T) + self.biases
+        product = np.dot(self._weights, inputs.T) + self._biases
         result = []
-        for i in range(len(self.activations)):
-            result.append(self.activations[i](product[i]))
+        for i in range(len(self._activations)):
+            result.append(self._activations[i](product[i]))
 
         return result
+    
+    def set_weights(self, w: list) -> None:
+        self._weights = np.array(w, dtype=np.float64)
+    
+    def get_weights(self):
+        return self._weights
+    
+    def set_biases(self, biases: list):
+        self._biases = np.array(biases, dtype=np.float64)
+    
+    def get_biases(self):
+        return self._biases
+
+    def set_activations(self, activations: list):
+        self._activations = activations
+    
+    def get_activations(self):
+        return self._activations
